56c21f16cb16b827abe464377ed7cb02607c339e
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-07-31 02:19:51 -0500

    feat: uninstall innecesary dependencies form photoswipe,, and aria-labels added to updgrade the accesibility

--- proyecto/tailwind.config.mjs
+++ proyecto/tailwind.config.mjs
@@ -1,26 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'gris': {
-					DEFAULT: '#DFDFDF',
-				},
-				'blanco': {
-					DEFAULT: '#FFFFFF',
-				},
-				'rojo': {
-					DEFAULT: '#C42626',
-				},
-				'Titulos': {
-					DEFAULT: '#231F20',
-				},
-				'Parrafos': {
-					DEFAULT: '#251F21',
-				},
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        gris: {
+          DEFAULT: "#DFDFDF",
+        },
+        blanco: {
+          DEFAULT: "#FFFFFF",
+        },
+        rojo: {
+          DEFAULT: "#C42626",
+        },
+        Titulos: {
+          DEFAULT: "#231F20",
+        },
+        Parrafos: {
+          DEFAULT: "#251F21",
+        },
+      },
+      screens: {
+        xs: "400px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+  },
+  plugins: [],
+};
