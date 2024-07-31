a86503da00df8db2084341988a1524e8135a328f
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-07-30 19:17:33 -0500

    feat: upgrade image formats to webp, aria-labels and view transitions added

--- proyecto/src/data/visits-cards.ts
+++ proyecto/src/data/visits-cards.ts
@@ -9,14 +9,14 @@ interface CardVisit {
 // Aqui almacenaremos la data de los cards de manera dinámica
 export const cards: CardVisit[] = [
     {
-        "visit_img": "../../Imagenes/visit-llanday.png",
+        "visit_img": "../../Imagenes/visit-llanday.webp",
         "title": "Visita Técnica Llinday",
         "description": "La visita técnica a las instalaciones de Llanday ofrece una oportunidad única para adentrarse en el mundo de la agricultura de precisión y la automatización agrícola. Durante la visita, los participantes tendrán la oportunidad de explorar de primera mano las tecnologías avanzadas utilizadas en la gestión de cultivos y la optimización de procesos agrícolas.",
         "visit_date": "29/06/19",
         "link_url": "https://www.google.com.pe"
     },
     {
-        "visit_img": '../../Imagenes/visit-backus.png',
+        "visit_img": '../../Imagenes/visit-backus.webp',
         "title": "Visita Técnica Backus",
         "description": "La visita técnica a la planta de Backus ofrece una oportunidad única para sumergirse en el mundo de la robótica aplicada a la industria cervecera. Durante la visita, los participantes tendrán la oportunidad de interactuar con una variedad de robots industriales que desempeñan un papel crucial en diferentes etapas del proceso de producción.",
         "visit_date": "31/06/24",
--- proyecto/src/data/workshops-cards.ts
+++ proyecto/src/data/workshops-cards.ts
@@ -10,7 +10,7 @@ interface CardWorkshop {
 // Aqui almacenaremos la data de los cards de manera dinámica
 export const cards: CardWorkshop[] = [
     {
-        "workshop_img": '../../Imagenes/python-workshop.png',
+        "workshop_img": '../../Imagenes/python-workshop.webp',
         "title": "Programación con Python",
         "description": "Explora el poder de Python en nuestro taller. Desde principiantes hasta expertos, descubre cómo escribir código eficiente, crear aplicaciones web y automatizar tareas",
         "profile_img": 'https://www.show.news/__export/1693849403093/sites/debate/img/2023/09/04/nicola-porcella.jpg_943222218.jpg',
