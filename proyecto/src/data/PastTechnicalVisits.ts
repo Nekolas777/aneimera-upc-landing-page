c8369526c5dff7818e2cf05cc92c3072c7c4c790
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-08-04 18:18:23 -0500

    update: texts for events and placeholders removed

---
+++ proyecto/src/data/PastTechnicalVisits.ts
@@ -0,0 +1,24 @@
+interface CardPastTechnicalVisits {
+  visit_img: string;
+  title: string;
+  visit_date?: string;
+  description: string;
+  link_url: string;
+}
+
+export const cards: CardPastTechnicalVisits[] = [
+  {
+      "visit_img": "../../Imagenes/visit-llanday.webp",
+      "title": "Visita Técnica Llinday",
+      "description": "La visita técnica a las instalaciones de Llanday ofrece una oportunidad única para adentrarse en el mundo de la agricultura de precisión y la automatización agrícola. Durante la visita, los participantes tendrán la oportunidad de explorar de primera mano las tecnologías avanzadas utilizadas en la gestión de cultivos y la optimización de procesos agrícolas.",
+      "visit_date": "29/06/2019",
+      "link_url": "https://www.facebook.com/profile.php?id=61556567363828"
+  },
+  {
+      "visit_img": '../../Imagenes/visit-backus.webp',
+      "title": "Visita Técnica Backus",
+      "description": "La visita técnica a la planta de Backus ofrece una oportunidad única para sumergirse en el mundo de la robótica aplicada a la industria cervecera. Durante la visita, los participantes tendrán la oportunidad de interactuar con una variedad de robots industriales que desempeñan un papel crucial en diferentes etapas del proceso de producción.",
+      "visit_date": "31/06/2024",
+      "link_url": "https://www.facebook.com/profile.php?id=61556567363828"
+  }
+]
--- proyecto/src/data/WorkShopCards.ts
+++ proyecto/src/data/WorkShopCards.ts
@@ -6,6 +6,7 @@ interface CardWorkshop {
     name: string;
     date: string;
     status: string;
+    form_link?: string;
 }
 
 // Aqui almacenaremos la data de los cards de manera dinámica
@@ -17,7 +18,8 @@ export const cards: CardWorkshop[] = [
         "profile_img": 'https://www.show.news/__export/1693849403093/sites/debate/img/2023/09/04/nicola-porcella.jpg_943222218.jpg',
         "name": "Dax Collas",
         "date": "29/06/2019",
-        "status": "Experto en HTML"
+        "status": "Experto en HTML",
+        "form_link": "https://docs.google.com/forms/u/0/?tgif=d&ec=asw-forms-globalnav-goto"
     },
     {
         "workshop_img": 'https://www.uch.edu.pe/sites/default/files/styles/blog_image/public/blog-img/arduino.jpg?itok=sWaQF4DE',
@@ -26,7 +28,8 @@ export const cards: CardWorkshop[] = [
         "profile_img": '../../Imagenes/profile-one.png',
         "name": "Luis Reyes",
         "date": "29/06/2019",
-        "status": "Experto en HTML"
+        "status": "Experto en HTML",
+        "form_link": "https://docs.google.com/forms/u/0/?tgif=d&ec=asw-forms-globalnav-goto"
     },
     {
         "workshop_img": 'https://ammitechnologies.com/wp-content/uploads/2022/07/AdobeStock_16091753.jpg',
@@ -35,7 +38,8 @@ export const cards: CardWorkshop[] = [
         "profile_img": 'https://avatars.githubusercontent.com/u/129230632?v=4',
         "name": "Sebastian Ramirez",
         "date": "29/06/2019",
-        "status": "Experto en HTML"
+        "status": "Experto en HTML",
+        "form_link": "https://docs.google.com/forms/u/0/?tgif=d&ec=asw-forms-globalnav-goto"
     },
     {
         "workshop_img": 'https://mundoenlinea.cl/wp-content/uploads/2020/08/Robotica-y-Domotica.jpg',
@@ -44,6 +48,7 @@ export const cards: CardWorkshop[] = [
         "profile_img": 'https://i.pinimg.com/736x/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg',
         "name": "Arian Rodriguez",
         "date": "29/06/2019",
-        "status": "Experto en HTML"
-    }
+        "status": "Experto en HTML",
+        "form_link": "https://docs.google.com/forms/u/0/?tgif=d&ec=asw-forms-globalnav-goto"
+    },
 ]
