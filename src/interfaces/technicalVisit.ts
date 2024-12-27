40b48b6f654bba79087ee9fcf1087ae21eb71761
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-12-27 16:05:53 -0500

    feat: fetch to APIS and display response for Ponencias, VIsitas and Talleres

---
+++ src/interfaces/technicalVisit.ts
@@ -0,0 +1,12 @@
+export interface TechnicalVisit {
+  visitaTecninaId: number,
+  titulo: string,
+  descripcion: string,
+  fecha: Date,
+  hora: string,
+  aforo: string,
+  modalidad: string,
+  enlace: string,
+  rutaImagen: string,
+  estado: number,
+}
