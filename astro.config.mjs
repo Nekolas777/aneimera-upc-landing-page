adc440593e4d8326e583bbbe87087863761c16b3
Nicolas Esteban <gokuparapresidente10@hotmail.com>
2025-03-13 01:22:58 -0500

    chore: schamea config for public .env variables for ANEIMERA_API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
@@ -7,6 +7,15 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
+  env: {
+    schema: {
+      ANEIMERA_API: envField.string({
+        context: 'client',
+        access: 'public',
+        optional: false
+      })
+    }
+  },
   adapter: vercel({
     webAnalytics: {
       enabled: true,
